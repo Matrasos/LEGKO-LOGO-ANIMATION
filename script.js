b61d4a1f73d5e22a9e9c6addfdc47c4c3fd24e98
Matrasos <sh.zhenis@yandex.kz>
2024-08-19 15:30:34 +0500

    logo animation

---
+++ script.js
@@ -0,0 +1,30 @@
+gsap.fromTo(
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 3.5,
+  }
+);
+
+gsap.fromTo(
+  "#svg path",
+  { strokeDashoffset: 4500, fillOpacity: 0 },
+  { strokeDashoffset: 0, fillOpacity: 1, duration: 6, ease: "power1.inOut" }
+);
+
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0.5,
+  }
+);
